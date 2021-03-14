f0f9523fb5e932582a2703fe7a407b1d3f9b629f
Peter Pham <peterpham86@gmail.com>
2021-03-13 20:20:12 -0800

    add better comments to installation script

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-:: BatchGotAdmin
+::  prompt user to get elevated priviledge for installation
 ::-------------------------------------
 REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
@@ -26,14 +26,7 @@ if '%errorlevel%' NEQ '0' (
 
 ::ENTER YOUR CODE BELOW:
 
-::Download tdm-gcc run time
-ECHO Please install TDM-GCC runtime first: 
-ECHO https://github.com/jmeubank/tdm-gcc/releases/download/v9.2.0-tdm64-1/tdm64-gcc-9.2.0.exe
-@echo off
-setlocal
-:PROMPT
-SET /P AREYOUSURE=Did you install required GCC runtime (Y/[N])?
-IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+:: Delete windows service service if it exists and create new one
 SC QUERY EFIBootSelectorService > NUL
 IF ERRORLEVEL 1060 GOTO MISSING
 ECHO service exist stopping then deleting service
@@ -46,27 +39,28 @@ ECHO service does not exist creating new one...
 
 :NOSERVICE
 
-taskkill /IM eficlient.exe
+:: kill any existing client
+QPROCESS "eficlient.exe">NUL 2> nul
+IF %ERRORLEVEL% EQU 0 taskkill /IM eficlient.exe
+
 
 timeout 2
 
+::Copy binaries to target destination
 if exist "c:\efibootselector\" rd /q /s "c:\efibootselector
 mkdir c:\efibootselector
 xcopy efiserver.exe c:\efibootselector\
 xcopy eficlient.exe c:\efibootselector\
 xcopy efiDLL.dll c:\efibootselector\
 
-
+::Create windows service
 sc create EFIBootSelectorService binPath="C:\efibootselector\efiserver.exe"
 sc config EFIBootSelectorService start= auto
 sc start EFIBootSelectorService 
 
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v EFIBootSelector /d "C:\Windows\System32\cmd.exe /k start C:\efibootselector\eficlient.exe" /f 
+:: add registry key to auto start eficlient on windows startup
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v EFIBootSelector /d "C:\Windows\System32\cmd.exe /k start C:\efibootselector\eficlient.exe" /f
 
 timeout 5
 start C:\efibootselector\eficlient.exe
-
-:END
-endlocal
-
 pause
